7aa84cae3508b6572893bb7717feac1eb4e830e7
dimihtsk <dimihtsk@gmail.com>
2024-11-06 01:34:52 +0000

    testing: testing new configuration

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     preset: 'react-native',
     collectCoverage: true,
-    coverageDirectory: 'coverage',
+    collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!**/node_modules/**'],
+    coverageDirectory: './coverage',
     coverageReporters: ['lcov', 'text'],
 }
