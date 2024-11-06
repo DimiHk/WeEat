33b7ade9f26bd58526027a614b9c0c00c55b3fbb
dimihtsk <dimihtsk@gmail.com>
2024-11-06 01:20:56 +0000

    testing: testing new configuration

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -2,12 +2,10 @@ import 'react-native'
 import React from 'react'
 import App from '../App'
 
-import {expect, it} from '@jest/globals'
+import {it} from '@jest/globals'
 
 import renderer from 'react-test-renderer'
 
 it('renders correctly', () => {
-    const tree = renderer.create(<App />).toJSON()
-
-    expect(tree).toHaveProperty('nonExistentProperty')
+    renderer.create(<App />)
 })
